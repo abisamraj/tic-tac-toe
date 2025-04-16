3654c26706c0ef3a9e4a60e0b473e4536fd8b30c
abisamraj <abisamraj03@gmail.com>
2025-04-16 11:58:36 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: 'https://github.com/abisamraj/tic-tac-toe',
+  // plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
